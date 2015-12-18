502a5affa207e105de56127ce4aae242cb33dee9
oleksandrlysachenko <lysachenko.oleksandr@gmail.com>
2015-12-18 16:38:43 +0200

    Add config file

---
+++ config/development.js
@@ -0,0 +1,6 @@
+process.env.HTTP_HOST = 'http://localhost:3030/';
+process.env.HTTP_PORT = 3030;
+process.env.DB_HOST = 'localhost';
+
+process.env.DB_NAME = 'VrakashyDB';
+process.env.DB_PORT = 27017;
--- server.js
+++ server.js
@@ -1,62 +1,49 @@
 var express = require('express');
+var app = express();
 var mongoose = require('mongoose');
 var db = mongoose.connection;
+var http = require('http');
+var server = http.createServer(app);
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var mongoStore = require('connect-mongo')(session);
-var app = express();
-var port = 3030;
-var dbConfig = {
 
-};
-
-mongoose.connect('localhost','VrakashyDB',27017);
+require('./config/development.js');
 
 app.use(cookieParser());
-app.use(session({
-   key: 'sessionID',
-   secret: 'abcd1234few',
-   resave: true,
-   saveUninitialized: true,
-   store: new mongoStore({
-      host: 'localhost',
-      port: 27017,
-      db: 'VrakashyDB',
-      collection: 'session'
-   })
-}));
-/*app.use(function(req,res,next){
-   var cook = req.cookies.cookieName;
-   if (cook === undefined)
-   {
-      // no: set a new cookie
-      var randomNumber=Math.random().toString();
-      randomNumber=randomNumber.substring(2,randomNumber.length);
-      res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
-      console.log('cookie created successfully');
-   }
-   else
-   {
-      // yes, cookie was already present
-      console.log('cookie exists', cook);
-   }
-   next(); // <-- important!
-}); */
 app.use(express.static(__dirname + '/public'));
+
+mongoose.connect(process.env.DB_HOST, process.env.DB_NAME, process.env.DB_PORT);
 
 db.once('open', function() {
 
-   require('./models');
-   require('./routes/index')(app);
+   console.log('Connection to ' + process.env.DB_NAME + ' is success');
+   //console.log(process);
 
-   app.use(express.static(__dirname + '/public'));
 
-   app.listen(port, function () {
-      console.log('-> Server started on port: ' + port);
+   app.use(session({
+      //key: 'sessionID',
+      secret: 'abcd1234few',
+      resave: true,
+      saveUninitialized: true,
+      store: new mongoStore({
+         host: process.env.DB_HOST,
+         port: process.env.DB_PORT,
+         db: process.env.DB_NAME,
+         collection: 'Sessions',
+         ssl: false,
+         autoReconnect: true
+      })
+   }));
+
+   //require('./routes/index')(app);
+
+   server.listen(process.env.HTTP_PORT, function () {
+      console.log('-> | Server started on port: ' + process.env.HTTP_PORT);
+      console.log('   | Host: ' + process.env.HTTP_HOST);
+      console.log('   | Database host: ' + process.env.DB_HOST)
    });
 });
 
-db.on('error', function(err) {
-   console.error(err);
-});
+db.on('error', console.error.bind(console, 'connection error:'));
