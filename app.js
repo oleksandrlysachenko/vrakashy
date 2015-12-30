e29a73e21c974d2aacb4ea3d6fe1d1963ee1195d
oleksandrlysachenko <lysachenko.oleksandr@gmail.com>
2015-12-30 11:44:56 +0200

    Add multiply cluster

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var mongoStore = require('connect-mongo')(session);
+var serverLog = require('morgan');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+
+require('./config/development');
+
+mongoose.connect(process.env.DB_HOST, process.env.DB_NAME, process.env.DB_PORT);
+
+app.use(cookieParser());
+app.use(express.static(__dirname + '/public'));
+app.use(serverLog('dev'));
+app.use(bodyParser.json({ strict: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+db.once('open', function() {
+
+    console.log('Connection to ' + process.env.DB_NAME + ' is succeed');
+
+    app.use(session({
+        secret: 'abcd1234few',
+        resave: true,
+        saveUninitialized: true,
+        store: new mongoStore({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            db: process.env.DB_NAME,
+            collection: 'Sessions'
+        })
+    }));
+
+    //require('./routes/index')(app, db);
+
+    app.get('/', function (req, res, next) {
+        res.status(200).send('Process ' + process.pid + ' work!');
+    });
+
+    server.listen(process.env.HTTP_PORT, function () {
+        console.log('-> Server start succeed!');
+        console.log('   | Server host: ' + process.env.HTTP_HOST);
+        console.log('   | Server port: ' + process.env.HTTP_PORT);
+        console.log('   | Database host: ' + process.env.DB_HOST);
+        console.log('   | Database port: ' + process.env.DB_PORT);
+    });
+});
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,49 +1,30 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var db = mongoose.connection;
-var http = require('http');
-var server = http.createServer(app);
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
-var mongoStore = require('connect-mongo')(session);
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
-require('./config/development.js');
+if (cluster.isMaster) {
 
-app.use(cookieParser());
-app.use(express.static(__dirname + '/public'));
+    console.log('Master cluster settings up ' + numCPUs + 'CPUs');
 
-mongoose.connect(process.env.DB_HOST, process.env.DB_NAME, process.env.DB_PORT);
+    // TODO uncomment for set up ONE worker!
+    //cluster.fork();
 
-db.once('open', function() {
+    // TODO uncomment for set up MORE worker!
+    for (var i = 0; i < numCPUs; i++) {
+        setTimeout(function() {
+            cluster.fork()
+        }, 1000);
+    }
 
-   console.log('Connection to ' + process.env.DB_NAME + ' is success');
-   //console.log(process);
+    cluster.on('online', function (worker) {
+        console.log('Worker ' + worker.process.pid + ' is online')
+    });
 
+    cluster.on('exit', function (worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + 'died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker!');
+        cluster.fork();
+    });
+} else {
+    require('./app');
+}
 
-   app.use(session({
-      //key: 'sessionID',
-      secret: 'abcd1234few',
-      resave: true,
-      saveUninitialized: true,
-      store: new mongoStore({
-         host: process.env.DB_HOST,
-         port: process.env.DB_PORT,
-         db: process.env.DB_NAME,
-         collection: 'Sessions',
-         ssl: false,
-         autoReconnect: true
-      })
-   }));
-
-   //require('./routes/index')(app);
-
-   server.listen(process.env.HTTP_PORT, function () {
-      console.log('-> | Server started on port: ' + process.env.HTTP_PORT);
-      console.log('   | Host: ' + process.env.HTTP_HOST);
-      console.log('   | Database host: ' + process.env.DB_HOST)
-   });
-});
-
-db.on('error', console.error.bind(console, 'connection error:'));
